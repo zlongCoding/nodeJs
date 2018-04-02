eda6a0dc30c058d7c962e5db39debaff05538955
zhanglongdream <zhanglong_hn@163.com>
2018-04-02 10:20:10 +0800

    文件格式补充

--- 文件下载/express.js
+++ 文件下载/express.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var fs=require("fs");
+var express = require('express')
+var fs=require("fs")
 
-var app = express();
+var app = express()
 app.get('/download', function (req, res, next) {
-  var path="/Users/app-release.apk";
-  var f = fs.createReadStream(path);
+  var path="/Users/app-release.apk"
+  var f = fs.createReadStream(path)
   res.writeHead(200, {
     'Content-Type': 'application/force-download',
-    'Content-Disposition': 'attachment; filename=NW.js.docx'
-  });
-  f.pipe(res);
-});
+    'Content-Disposition': 'attachment filename=NW.js.docx'
+  })
+  f.pipe(res)
+})
 
 app.listen(3000)
